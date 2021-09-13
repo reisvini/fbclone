82d083b1593545d2a212d3dc16e8216b0b435fcf
Vinicius Reis <vinicius.moreira2003@gmail.com>
2021-09-13 20:58:16 -0300

    NextAuth and finishing Header

---
+++ components/HeaderIcon.tsx
@@ -0,0 +1,16 @@
+interface HeaderIconProps {
+  Icon: any;
+  active?: boolean;
+}
+
+export default function HeaderIcon({ Icon, active }: HeaderIconProps) {
+  return (
+    <button className="flex items-center cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl group active:border-b-2 active:">
+      <Icon
+        className={`h-5 text-gray-500 text-center sm:h-7 mx-auto group-hover:text-blue-500 
+        ${active && 'text-blue-500'}
+        `}
+      />
+    </button>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import { getSession } from 'next-auth/client';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Login from '../components/Login';
+import Sidebar from '../components/Sidebar';
 
-export default function Home() {
+export default function Home({ session }) {
+  if(!session) {
+    return <Login />
+  }
+
   return (
     <div>
       <Head>
@@ -9,7 +16,19 @@ export default function Home() {
       </Head>
 
       <Header />
-      <main></main>
+      <main>
+        <Sidebar />
+      </main>
     </div>
   );
+}
+
+export async function getServerSideProps(ctx) {
+  const session = await getSession(ctx);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
