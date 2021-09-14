869a4ae40c3e4651e21f81ec16c959aa76bbec14
Vinicius Reis <vinicius.moreira2003@gmail.com>
2021-09-14 19:05:15 -0300

    Finishing Post Footer

--- components/PostComponents/Post.tsx
+++ components/PostComponents/Post.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { ChatAltIcon, ShareIcon, ThumbUpIcon } from '@heroicons/react/outline';
 
 type PostProps = {
   key;
@@ -51,6 +52,23 @@ export default function Post({
           />
         </div>
       )}
+
+      <div className="flex justify-between items-center rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
+        <div className="inputIcon rounded-none rounded-bl-2xl">
+          <ThumbUpIcon className="h-4" />
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+
+        <div className="inputIcon rounded-none ">
+          <ChatAltIcon className="h-4" />
+          <p className="text-xs sm:text-base">Comment</p>
+        </div>
+
+        <div className="inputIcon rounded-none rounded-br-2xl">
+          <ShareIcon className="h-4" />
+          <p className="text-xs sm:text-base">Share</p>
+        </div>
+      </div>
     </div>
   );
 }
